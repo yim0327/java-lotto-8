1e60a4aecf27b4d969996094994477d49b258761
yim0327 <yim0327@naver.com>
2025-11-01 21:50:09 +0900

    feat(lottoMachine): 로또 발행 구현 및 로또 묶음 생성
    - LottoBundle
      - 여러 Lotto 객체들을 리스트에 담아 일급 컬렉션으로 구현
    
    - LottoMachine
      - issueLotto(int lottoCount) 메서드를 통해 발행 개수만큼 로또 발행
      - 주입받은 생성기를 통해 Lotto 객체를 생성하여 리스트에 저장
      - 저장한 리스트를 이용하여 LottoBudle 생성 후 반환

---
+++ src/main/java/lotto/domain/LottoBundle.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoBundle {
+    private final List<Lotto> lottos;
+
+    public LottoBundle(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+}
--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -2,11 +2,24 @@ package lotto.domain;
 
 import lotto.domain.generator.NumbersGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
     private final NumbersGenerator generator;
 
     public LottoMachine(NumbersGenerator generator) {
         this.generator = generator;
+    }
+
+    public LottoBundle issueLotto(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(generator.generate()));
+        }
+
+        return new LottoBundle(lottos);
     }
 
 }

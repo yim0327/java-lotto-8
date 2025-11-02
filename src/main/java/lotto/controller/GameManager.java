63eeda0d2befc4d6303648e816f65b60527bd875
yim0327 <yim0327@naver.com>
2025-11-03 01:29:00 +0900

    feat(controller): lottoMachine 주입 및 로또 묶음 객체 생성
    - GameManager에 로또 묶음 생성 책임을 지닌 LottoMachine 주입
    - 로또 머신의 issueLotto 메서드에 로또 개수를 넘겨, 해당하는 만큼 로또
      발행 후 로또 묶음으로 반환 받음

--- src/main/java/lotto/controller/GameManager.java
+++ src/main/java/lotto/controller/GameManager.java
@@ -1,16 +1,21 @@
 package lotto.controller;
 
+import lotto.domain.LottoBundle;
+import lotto.domain.LottoMachine;
 import lotto.util.InputParser;
 import lotto.view.InputView;
 
 public class GameManager {
     private final InputView inputView;
+    private final LottoMachine lottoMachine;
 
-    public GameManager(InputView inputView) {
+    public GameManager(InputView inputView, LottoMachine lottoMachine) {
         this.inputView = inputView;
+        this.lottoMachine = lottoMachine;
     }
 
     public void start() {
         int lottoCount = new InputParser(inputView.inputPurchaseAmount()).parseInt();
+        LottoBundle lottoBundle = lottoMachine.issueLotto(lottoCount);
     }
 }

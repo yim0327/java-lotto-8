8f7e8bf5e8a5232f67eeb126f6649349e18375f1
yim0327 <yim0327@naver.com>
2025-11-03 22:28:55 +0900

    feat(lottoBundle): 로또 묶음 내 로또 결과 판별 로직 구현
    - 각 로또들을 당첨 번호와 모두 매칭하여 Rank 결과를 Map으로 반환
    - EnumMap을 사용하여 빠르고 효율적인 탐색 및 결과 도출

--- src/main/java/lotto/domain/LottoBundle.java
+++ src/main/java/lotto/domain/LottoBundle.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoBundle {
     private final List<Lotto> lottos;
@@ -13,4 +15,31 @@ public class LottoBundle {
         return lottos.size();
     }
 
+    /**
+     * 결과 판별 : 각 로또들을 모두 매칭하여 결과 Map을 반환
+     * - 로또 객체 리스트(상태)를 가지고 있는 LottoBundle에게 판별 책임(행동) 부여
+     */
+    public Map<Rank, Integer> judgeLotto(WinningNumber winningNumber) {
+        Map<Rank, Integer> counts = makeEnumMap();
+
+        for (Lotto lotto : lottos) {
+            Rank rank = winningNumber.match(lotto);
+            counts.merge(rank, 1, Integer::sum); // 없으면 새로 추가, 있으면 +1 갱신
+        }
+
+        return counts;
+    }
+
+    /**
+     * EnumMap : Key를 Enum으로 하는 Map 컬렉션 생성 및 초기화
+     */
+    private Map<Rank, Integer> makeEnumMap() {
+        // EnumMap : Enum타입만을 key로 사용하는 특수한 Map
+        Map<Rank, Integer> map = new EnumMap<>(Rank.class);
+        for (Rank r : Rank.values()) {  // EnumMap 초기화
+            map.put(r, 0);
+        }
+
+        return map;
+    }
 }

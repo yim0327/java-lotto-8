96bfa16d34e948ef0eebaaa6486aedc27f20b62f
yim0327 <yim0327@naver.com>
2025-11-03 23:27:48 +0900

    refactor(outputView): 수익률 출력 형식 수정
    - 출력 시 rate 값을 소수점 둘째 자리까지 표시하도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -24,6 +24,6 @@ public class OutputView {
     }
 
     public void printProfit(double profitRate) {
-        System.out.println("총 수익률은 "+String.format("%.2f%%", profitRate)+"입니다.");
+        System.out.println("총 수익률은 "+String.format("%.1f%%", profitRate)+"입니다.");
     }
 }

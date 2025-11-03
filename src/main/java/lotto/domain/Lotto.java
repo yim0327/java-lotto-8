d9a47805633c503d9ce3bb43ee8d1508044041ac
yim0327 <yim0327@naver.com>
2025-11-03 16:00:01 +0900

    feat(lotto): 로또 1~45 사이 숫자 검증 로직 추가
    - 리스트 내 값이 1보다 작거나 45보다 크면 예외 발생
    - 최소 범위 1을 MIN_NUMBER_BOUNDRIES, 최대 범위 45를
      MAX_NUMBER_BOUNDRIES로 불변 상수 설정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,11 +6,15 @@ import java.util.List;
 public class Lotto {
     private static final String INVALID_NUMBER_COUNT_ERROR = "[ERROR] 로또 번호는 6개여야 합니다.";
     private static final String INVALID_DUPLICATE_NUMBERS_ERROR = "로또 번호는 중복될 수 없습니다.";
+    private static final String INVALID_NUMBER_BOUNDARIES_ERROR = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final int MIN_NUMBER_BOUNDARIES = 1;
+    private static final int MAX_NUMBER_BOUNDARIES = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateNumberCount(numbers);
         validateDuplicateNumbers(numbers);
+        validateNumberBoundary(numbers);
         this.numbers = numbers;
     }
 
@@ -25,6 +29,13 @@ public class Lotto {
 
         if (set.size() != numbers.size()) {
             throw new IllegalArgumentException(INVALID_DUPLICATE_NUMBERS_ERROR);
+        }
+    }
+
+    private void validateNumberBoundary(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(num -> num < MIN_NUMBER_BOUNDARIES
+                || num > MAX_NUMBER_BOUNDARIES)) {
+            throw new IllegalArgumentException(INVALID_NUMBER_BOUNDARIES_ERROR);
         }
     }
 }

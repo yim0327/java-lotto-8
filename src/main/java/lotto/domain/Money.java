9b076e7c8c6d0a46493656f83ab19ca83fdddc9a
yim0327 <yim0327@naver.com>
2025-11-03 15:11:18 +0900

    refactor(money): 구입 금액 단위 검증 로직 구현
    - validateIsThousandUnit()을 통해, 구입 금액이 로또 가격으로 나누어
      떨어지지 않는 경우 IllegalArgumentException 발생

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 public class Money {
+    private static final String INVALID_AMOUNT_UNIT_ERROR = "구입 금액은 1,000원 단위로만 입력 가능합니다.";
     private static final int LOTTO_PURCHASE = 1000;
     private final int purchaseAmount;
 
@@ -13,5 +14,9 @@ public class Money {
         return purchaseAmount/LOTTO_PURCHASE;
     }
 
-    private void validateIsThousandUnit(int purchaseAmount) {}
+    private void validateIsThousandUnit(int purchaseAmount) {
+        if (purchaseAmount % LOTTO_PURCHASE != 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_UNIT_ERROR);
+        }
+    }
 }

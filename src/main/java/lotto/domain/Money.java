ee54abe08ef54b21a18fedb3186e36d511e83f7b
yim0327 <yim0327@naver.com>
2025-11-03 23:15:32 +0900

    feat(money): 구입 금액이 0보다 작을 시 예외 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
 public class Money {
-    private static final String INVALID_AMOUNT_UNIT_ERROR = "구입 금액은 1,000원 단위로만 입력 가능합니다.";
     private static final int LOTTO_PURCHASE = 1000;
+    private static final String INVALID_AMOUNT_UNIT_ERROR = "구입 금액은 "+LOTTO_PURCHASE+"원 단위로만 입력 가능합니다.";
+    private static final String INVALID_UNDER_ZERO_AMOUNT_ERROR = "구입 금액은 0보다 커야 합니다.";
     private final int purchaseAmount;
 
     public Money(int purchaseAmount) {
+        validateUnderZeroAmount(purchaseAmount);
         validateIsThousandUnit(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
     }
@@ -21,6 +23,12 @@ public class Money {
     private void validateIsThousandUnit(int purchaseAmount) {
         if (purchaseAmount % LOTTO_PURCHASE != 0) {
             throw new IllegalArgumentException(INVALID_AMOUNT_UNIT_ERROR);
+        }
+    }
+
+    private void validateUnderZeroAmount(int purchaseAmount) {
+        if (purchaseAmount <= 0) {
+            throw new IllegalArgumentException(INVALID_UNDER_ZERO_AMOUNT_ERROR);
         }
     }
 }

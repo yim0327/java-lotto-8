c7fe8644329a484632b0498f349ae52b3311bf6c
yim0327 <yim0327@naver.com>
2025-11-03 15:39:24 +0900

    feat(lotto): 로또 객체 중복 숫자 검증 추가 및 테스트 성공
    - 검증 메시지를 불변 상수로 선언
    - HashSet과 리스트의 길이 비교를 통한 중복 숫자 검증 추가
    - 기본 LottoTest 전체 성공

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,30 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+    private static final String INVALID_NUMBER_COUNT_ERROR = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String INVALID_DUPLICATE_NUMBERS_ERROR = "로또 번호는 중복될 수 없습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumberCount(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(INVALID_NUMBER_COUNT_ERROR);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>(numbers);
+
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException(INVALID_DUPLICATE_NUMBERS_ERROR);
+        }
+    }
 }

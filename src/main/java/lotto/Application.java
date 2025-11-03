2ba2b1918197f4b274ccac5beec6207bf5b428f2
yim0327 <yim0327@naver.com>
2025-11-03 18:18:16 +0900

    feat(Application): 객체 생성 및 start 메서드 실행
    - view의 InputView, domain의 RandomNumberGenerator 객체 생성
    - Generator 객체를 사용하여 LottoMachine 객체 생성
    - InputView와 LottoMachine 객체를 사용하여 GameManager 객체 생성
    - gameManager.start() 메서드 실행하여 로또 게임 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.GameManager;
+import lotto.domain.LottoMachine;
+import lotto.domain.generator.NumbersGenerator;
+import lotto.domain.generator.RandomNumbersGenerator;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        NumbersGenerator generator = new RandomNumbersGenerator();
+        LottoMachine lottoMachine = new LottoMachine(generator);
+        GameManager gameManager = new GameManager(inputView, lottoMachine);
+
+        gameManager.start();
     }
 }
